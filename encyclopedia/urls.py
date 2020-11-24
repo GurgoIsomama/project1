b7613f90f2fca818b552cbf8ef6424663330e6f4
Hannes Bengtsson Malmborg <hanma409@student.liu.se>
2020-11-24 15:13:55 +0100

    Done

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    #path("add", views.add, name="add"),
-    path("entry/<str:title>", views.entry, name="entry"),
+    path("add", views.add, name="add"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name ="search"),
+    path("random", views.random_page, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from encyclopedia.util import get_entry
+from encyclopedia.util import get_entry, save_entry
 from django import forms
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
@@ -6,8 +6,15 @@ from django.urls import reverse
 from django.core.files.base import ContentFile
 from . import util
 from markdown2 import Markdown
+import os, random
+
+import encyclopedia
 
 md = Markdown()
+
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(widget=forms.Textarea,label="Enter Markdowncontent Below")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -16,10 +23,64 @@ def index(request):
 
 
 def entry(request, title):
-    return render(request, "encyclopedia/entry.html", {
-        "entry": md.convert(util.get_entry(title)),
-        "title": title,
-    })
+    obj = util.get_entry(title)
+    if not obj:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": None,
+            "title" : title,
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": md.convert(obj),
+            "title": title,
+        })
+
+def search(request):
+    if request.method =="GET":
+        search = f"{request.GET.get('q')}"
+        entries = util.list_entries()
+        search_results = []
+        for filename in entries: 
+            if filename==search:
+                return entry(request, search)
+            elif search in filename:
+                search_results.append(filename)
+        return render(request, "encyclopedia/search.html", {
+        "search_results" : search_results,
+        "search" : search
+        })
+    else:        
+        return render(request, "encyclopedia/search.html", {
+            "search" : "method in search.html != GET "
+        })
+
+def add(request):
+    if request.method == "POST":
+            form = NewPageForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data["title"]
+                content = form.cleaned_data["content"]
+                obj = util.get_entry(title)
+                if not obj:
+                    save_entry(title, content)
+                    return entry(request, title)
+                else:
+                   return render(request, "encyclopedia/add.html", {
+                    "form": form,
+                    "error": True,
+                }) 
+            else:
+                return render(request, "encyclopedia/add.html", {
+                    "form": form
+                })
+    else:
+        return render(request, "encyclopedia/add.html", {
+            "form": NewPageForm()
+        })
 
 
-# def add(request):
+def random_page(request):
+    entries = util.list_entries() 
+    filename = random.choice(entries)
+    return entry(request, filename)
+
